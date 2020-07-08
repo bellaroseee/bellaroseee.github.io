52afe92451815bb169bda1bd91271501c2e5d89f
bella <brose714@cs.washington.edu>
2020-07-08 13:23:45 -0700

    Updates

---
+++ precache-manifest.b97d4dfa737e4ed71a753d3f4d43fce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190fe88721dbd33c33668ff10d433762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b2b1c709b5b46ab96e5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ad2d5dcdc39708e98bb",
+    "url": "/static/js/2.d440e33b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d440e33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2b1c709b5b46ab96e5",
+    "url": "/static/js/main.4f1cb666.chunk.js"
+  },
+  {
+    "revision": "2cd9142fb33336235f54",
+    "url": "/static/js/runtime-main.bf09c8b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
